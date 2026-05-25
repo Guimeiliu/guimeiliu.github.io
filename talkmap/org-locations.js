962ac728e9a31159d770938f94fbb01dcf6224d8
Guimeiliu <93809811+Guimeiliu@users.noreply.github.com>
2026-05-25 17:45:55 +0200

    Update geocoded talk locations for talk map

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,42 @@
 var addressPoints = [
   [
-    "Conference Proceeding talk 3 on Relevant Topic in Your Field<br />Testing Institute of America 2014 Annual Conference; Los Angeles, CA",
-    34.0536909,
-    -118.242766
+    "The 15th Zhang Heng Academic Symposium<br />The 15th Zhang Heng Academic Symposium; Kunming, Yunnan, China",
+    25.0399353,
+    102.7169061
   ],
   [
-    "Talk 2 on Relevant Topic in Your Field<br />London School of Testing; London, UK",
-    51.5074456,
-    -0.1277653
+    "2024 Annual Workshop on 'Structure, Formation and Evolution of Star Clusters'<br />2024 Annual Workshop on 'Structure, Formation and Evolution of Star Clusters'; Hangzhou, Zhejiang, China",
+    30.2489634,
+    120.2052342
   ],
   [
-    "Tutorial 1 on Relevant Topic in Your Field<br />UC-Berkeley Institute for Testing Science; Berkeley, CA",
-    37.8708393,
-    -122.272863
+    "Annual Meeting of the Chinese Astronomical Society 2024<br />Annual Meeting of the Chinese Astronomical Society 2024; Hangzhou, Zhejiang, China",
+    30.2489634,
+    120.2052342
   ],
   [
-    "Talk 1 on Relevant Topic in Your Field<br />UC San Francisco, Department of Testing; San Francisco, CA",
-    37.7879363,
-    -122.4075201
+    "2025 Annual Workshop on 'Structure, Formation and Evolution of Star Clusters'<br />2025 Annual Workshop on 'Structure, Formation and Evolution of Star Clusters'; Dali, Yunnan, China",
+    25.6074778,
+    100.2651597
+  ],
+  [
+    "Workshop on Binary Stars and Related Science<br />Workshop on Binary Stars and Related Science; Urumqi, Xinjiang, China",
+    43.8244074,
+    87.6139038
+  ],
+  [
+    "IAUS 398: Compact Objects and Binaries in Dense Star Clusters<br />IAUS 398: Compact Objects and Binaries in Dense Star Clusters; Seoul, South Korea",
+    37.5666791,
+    126.9782914
+  ],
+  [
+    "Cluster Formation and Evolution Seminar<br />Cluster Formation and Evolution Seminar; Shanghai, China",
+    31.2312707,
+    121.4700152
+  ],
+  [
+    "Institute Seminar, University of Vienna<br />University of Vienna; Vienna, Austria",
+    48.2083537,
+    16.3725042
   ]
 ];
